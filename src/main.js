1b951485872cf3862f715b0f0e92217d42e0f934
draven <ztw1002@126.com>
2024-02-20 16:42:18 +0800

    finish login page

--- src/main.js
+++ src/main.js
@@ -1,15 +1,17 @@
 import '@/styles/common.scss'
-import {useIntersectionObserver} from '@vueuse/core'
-import {lazyPlugin} from "@/directives";
+import { lazyPlugin } from "@/directives";
 import { componentPlugin } from '@/components'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
-
-app.use(createPinia())
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
+app.use(pinia)
 app.use(router)
 app.use(lazyPlugin)
 app.mount('#app')
 app.use(componentPlugin)
+
--- src/views/Layout/components/LayoutNav.vue
+++ src/views/Layout/components/LayoutNav.vue
@@ -1,15 +1,43 @@
 <script setup>
+import router from "@/router";
+import { useUserStore } from "@/stores/userStore";
 
+
+const userStore = useUserStore();
+
+const confirm = () => {
+  userStore.clearUserInfo()
+  router.push('/login')
+}
+
+// //form实例统一校验
+// const formRef = ref(null);
+// const router = useRouter();
+// const doLogin = () => {
+//   formRef.value.validate(async (valid) => {
+//     // valid: 所有表单都通过校验  才为true
+//     //console.log(valid)
+//     if (valid) {
+//       //console.log(form.value)
+//       const { account, password } = form.value
+//       //const res = await loginAPI({account, password});
+//       const res = await userStore.getUserInfo({ account, password });
+//       //console.log(res)
+//       ElMessage({ type: 'success', message: '登录成功' })
+//       router.replace({ path: '/' })
+//     }
+//   })
+// }
 </script>
 
 <template>
   <nav class="app-topnav">
     <div class="container">
       <ul>
-        <template v-if="true">
-          <li><a href="javascript:;"><i class="iconfont icon-user"></i>周杰伦</a></li>
+        <template v-if="userStore.userInfo.token">
+          <li><a href="javascript:;"><i class="iconfont icon-user"></i>{{ userStore.userInfo.account }}</a></li>
           <li>
-            <el-popconfirm title="确认退出吗?" confirm-button-text="确认" cancel-button-text="取消">
+            <el-popconfirm title="确认退出吗?" @confirm="confirm" confirm-button-text="确认" cancel-button-text="取消">
               <template #reference>
                 <a href="javascript:;">退出登录</a>
               </template>
@@ -19,7 +47,7 @@
           <li><a href="javascript:;">会员中心</a></li>
         </template>
         <template v-else>
-          <li><a href="javascript:;">请先登录</a></li>
+          <li><a href="javascript:;" @click="$router.push('/login')">请先登录</a></li>
           <li><a href="javascript:;">帮助中心</a></li>
           <li><a href="javascript:;">关于我们</a></li>
         </template>
@@ -32,11 +60,13 @@
 <style scoped lang="scss">
 .app-topnav {
   background: #333;
+
   ul {
     display: flex;
     height: 53px;
     justify-content: flex-end;
     align-items: center;
+
     li {
       a {
         padding: 0 15px;
